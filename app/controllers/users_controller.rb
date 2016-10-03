bb55e7ef6739d2359ff779e086095d6550676e1f
masarufuruya <yannwennri2@gmail.com>
2016-10-03 14:00:45 +0000

    user-profile実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
+  before_action :redirect_unlogged_user, only: [:show, :edit, :update, :create]
+  
   def new
     @user = User.new
   end
   
   def show
     @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(update_params)
+      flash[:success] = '更新しました'
+      redirect_to @user
+    else
+      flash[:danger] = '更新に失敗しました'
+      render 'edit'
+    end
   end
   
   #ユーザー新規登録
@@ -20,7 +37,17 @@ class UsersController < ApplicationController
   end
   
   private
+    def redirect_unlogged_user
+      unless logged_in?
+        redirect_to root_path
+      end
+    end
+  
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def update_params
+      params.require(:user).permit(:name, :email, :description)
     end
 end
