512a8518da622cef24664e5974bcfa56d6fb9991
masarufuruya <yannwennri2@gmail.com>
2016-10-04 07:54:27 +0000

    add micropost model

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,3 @@ Rails.application.routes.draw do
   
   root to: "static_pages#home"
 end
-Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'signup', to: 'users#new'
-  resources :users
-  get    'login' , to: 'sessions#new'
-  post   'login' , to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-  
-  root to: "static_pages#home"
-end
